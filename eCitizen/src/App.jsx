8eb8e63478634c1b09bdf25d64a94b866aab2a3b
Victor-Mutwiri <mutwirilincoln19@gmail.com>
2024-01-24 16:49:05 +0300

    1.0

---
+++ eCitizen/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Navbar from './components/Navbar/navbar'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Navbar/>
+        <p>Hello</p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ eCitizen/src/components/Navbar/navbar.jsx
@@ -0,0 +1,19 @@
+export default function Navbar(){
+    return (
+        <header>
+            <a href="#" className="logo">
+                <img src="../../assets/Logo.png" alt="Logo"/>
+            </a>
+            <ul className="navbar">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Passports</a></li>
+                <li><a href="#">Visa</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Account</a></li>
+            </ul>
+        </header>
+    )
+}
