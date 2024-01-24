0b2f52d4577874e054c28b74a2c66525d55a3eb5
Victor-Mutwiri <mutwirilincoln19@gmail.com>
2024-01-24 22:58:43 +0300

    1.2

--- eCitizen/src/App.jsx
+++ eCitizen/src/App.jsx
@@ -9,7 +9,14 @@ function App() {
     <>
       <div>
         <Navbar/>
-        <p>Hello</p>
+        <section className='home'>
+          <div>
+            <h2>The Department of Immigration</h2>
+            <h3>Access Immigration services using your eCitizen account.</h3>
+            <p>Apply and pay for your Passport online and get Live updates of your application progress via SMS and Email.</p>
+            <button>Learn More</button>
+          </div>
+        </section>
         <Footer/>
       </div>
     </>
