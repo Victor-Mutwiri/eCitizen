16dc5003986b8a2af8c6bb9f8d7a809c410dce1f
Victor-Mutwiri <mutwirilincoln19@gmail.com>
2024-01-28 17:20:35 +0300

    2.4

--- eCitizen/src/components/Account/loginsignup.jsx
+++ eCitizen/src/components/Account/loginsignup.jsx
@@ -1,30 +1,37 @@
+import { useState } from 'react'
 import './loginsignup.css'
+import eCitizenLogo from '../../assets/Logo.png'
 
 export default function LoginSignup(){
+    const [action, setAction] = useState("Login")
     return (
-        <div className="container">
-            <div className="head">
-                <div className="text">Sign Up</div>
-                <div className="underline"></div>
-            </div>
-            <div className="inputs">
-                <div className="input">
-                    <label>Name</label>
-                    <input type="text" placeholder='Name...'/>
+        <div className="login-signup">
+            <img src={eCitizenLogo} alt="ImmigrationLogo" />
+            <div className="container">
+                <div className="head">
+                    <div className="text">{action}</div>
+                    <div className="underline"></div>
                 </div>
-                <div className="input">
-                    <label>Email</label>
-                    <input type="email" placeholder='Email ID...'/>
+                <div className="inputs">
+                    {action==="Login"?<div></div>:<div className="input">
+                        <i className='bx bx-user'></i>
+                        <input type="text" placeholder='Name...'/>
+                    </div>}
+                    
+                    <div className="input">
+                        <i className='bx bxs-envelope'></i>
+                        <input type="email" placeholder='Email ID...'/>
+                    </div>
+                    <div className="input">
+                        <i className='bx bx-lock-open'></i>
+                        <input type="password" placeholder='password...'/>
+                    </div>
                 </div>
-                <div className="input">
-                    <label>Password</label>
-                    <input type="password" placeholder='password...'/>
+                {action==="Sign Up"?<div></div>:<div className="forgot-password">Lost Password? <span>Click Here!</span></div>}            
+                <div className="submit-container">
+                    <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}} >Sign Up</div>
+                    <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
                 </div>
-            </div>
-            <div className="forgot-password">Lost Password? <span>Click Here!</span></div>
-            <div className="submit-container">
-                <div className="submit">Sign Up</div>
-                <div className="submit">Login</div>
             </div>
         </div>
     )
