23f2f1f5cc33eaf00d3ddc7b13817942a69763a3
Victor-Mutwiri <mutwirilincoln19@gmail.com>
2024-01-29 13:36:34 +0300

    2.5

--- eCitizen/src/components/Navbar/navbar.jsx
+++ eCitizen/src/components/Navbar/navbar.jsx
@@ -1,16 +1,16 @@
 import './navbar.css'
 import logoImage from '../../assets/Logo.png'
 import {Link} from 'react-router-dom'
-
+import {useState} from 'react'
 
 export default function Navbar(){
-
+    const [menuOpen, setMenuOpen] = useState (false)
     return (
         <header>
             <Link to="/" className="logo">
                 <img src={logoImage} alt="Logo"/>
             </Link>
-            <ul className="navbar">
+            <ul id="navbar" className={menuOpen?"open":""}>
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/services'>Services</Link></li>
                 <li><Link to='/passports'>Passports</Link></li>
@@ -20,7 +20,7 @@ export default function Navbar(){
                 <li><Link to='/contact Us'>Contact Us</Link></li>
                 <li><Link to='/account'>Account</Link></li>
             </ul>
-            <div className="bx bx-menu" id="menu-icon"></div>
+            <div className="bx bx-menu" id="menu-icon" onClick={()=>{setMenuOpen(!menuOpen)}}></div>
         </header>
     )
 }
